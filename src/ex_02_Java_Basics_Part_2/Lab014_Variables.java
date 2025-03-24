e3b296dfc826dc71896b2a736cd35adb365df92d
Ankit <goleankit8@gmail.com>
2025-03-24 21:39:32 +0530

    java basics part2

---
+++ src/ex_02_Java_Basics_Part_2/Lab014_Variables.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics_Part_2;
+
+public class Lab014_Variables {
+
+    public static void main(String[]args){
+        byte b= 10;
+        System.out.println(b);
+    }
+}
---
+++ src/ex_02_Java_Basics_Part_2/Lab016__Variables.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics_Part_2;
+
+public class Lab016__Variables {
+    public static void main(String[] args) {
+        int age=15;
+        age=age+15;
+        System.out.println(age);
+        age=76;
+        System.out.println(age);
+    }
+}
